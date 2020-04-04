9ebade3219132d90ba907658896841bfcb42869e
61637557 <lili@Lilis-MacBook-Air.local>
2020-04-04 23:10:39 +0100

    First commit

---
+++ src/components/Authentication/Login.vue
@@ -0,0 +1,34 @@
+<template>
+               <section class="content-info">
+
+                    <h2 class="user-links">
+                        <a class="active-route" href="login.html">Login</a> 
+                    </h2>
+
+                    <form class="user-form">
+                        <div class="form-group">
+                            <img src="https://img.icons8.com/material-sharp/42/000000/user.png">
+                            <input type="text" name="username" placeholder="Username">
+                        </div>
+
+                        <div class="form-group">
+                            <img src="https://img.icons8.com/material/42/000000/password--v1.png">
+                            <input type="password" name="password" placeholder="Password">
+                        </div>
+
+                        <button>Login</button>
+                    </form>
+                </section>
+</template>
+
+<script>
+export default {
+    components: {
+    }
+}
+</script>
+
+
+
+<style scoped>
+</style>
---
+++ src/components/core/Navigation.vue
@@ -0,0 +1,47 @@
+<template>
+<div> 
+<div class="navigation">
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">All trips</a>
+                    </li>
+                    <li>
+                        <a href="#">Create trip</a>
+                    </li>
+                    <li>
+                        <a href="#">Your wish list</a>
+                    </li>
+ 
+                </ul>
+
+                <ul>
+                    <li>
+                        <a href="#">Login</a>
+                    </li>
+                    <li>
+                        <a href="#">Registration</a>
+                    </li>
+                    <li>
+                        <a href="#">Logout</a>
+                    </li>
+                </ul>
+            </div>
+</div>
+</template>
+
+
+
+<script>
+export default {
+}
+</script>
+
+
+
+<style scoped>
+</style>
+
+
